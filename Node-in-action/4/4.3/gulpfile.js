74662d2aaf9245764fbdbb444f89a1d74365bced
yyyzzz674 <yz624557674@163.com>
2019-10-28 20:36:01 +0800

    gulp搭建

---
+++ Node-in-action/4/4.3/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
+const watch = require('gulp').watch
+gulp.task('build', () => {
+  return gulp
+    .src('app/*.jsx')
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/env', '@babel/react']
+      })
+    )
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+})
+gulp.task('watch', () => {
+  return watch('app/**.jsx', gulp.series('build'))
+})
+
+gulp.task('default', gulp.series('watch'))
