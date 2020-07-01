1f6f01fa5113c8a112f5a845dbb2fdc872629864
yyyzzz674 <yz624557674@163.com>
2020-07-01 12:30:23 +0800

    添加Watcher.js

--- 深入浅出Vue.js/第2章Object的变化侦测/利用vue实现原理实现mvvm/Watcher.js
+++ 深入浅出Vue.js/第2章Object的变化侦测/利用vue实现原理实现mvvm/Watcher.js
@@ -5,13 +5,13 @@ function Watcher(vm, prop, callback) {
   this.value = this.get()
 }
 Watcher.prototype = {
-  get: function() {
+  get: function () {
     Dep.target = this
     const value = this.vm.$data[this.prop]
     Dep.target = null
     return value
   },
-  update: function() {
+  update: function () {
     const value = this.vm.$data[this.prop]
     const oldValue = this.value
     if (value !== oldValue) {
